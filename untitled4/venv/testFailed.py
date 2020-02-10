9d8d365d7ff92e8e3f8c956f3d07fe7e93c003f3
Sahron, Maymon <sharonelia123@gmail.com>
2020-02-10 14:46:58 +0200

    Sharon

---
+++ untitled4/venv/testFailed.py
@@ -0,0 +1,7 @@
+import pytest
+def test_a():
+    assert 0
+def test_B():
+    assert 1
+def test_C():
+    assert 1
---
+++ untitled4/venv/testPass.py
@@ -0,0 +1,5 @@
+import pytest
+def test_B():
+    assert 1
+def test_C():
+    assert 1
